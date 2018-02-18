65d42f94dbd99a314dbd8eacdd40960c059e5d56
vladaindjic <vladaindjic@icloud.com>
2018-02-18 21:35:18 +0100

    testiranje selective search; rezultati sliding window i selective search

---
+++ selective-search-test.py
@@ -0,0 +1,200 @@
+import cnn
+from keras.models import load_model
+from cnn import read_image
+from cnn import resize_and_save
+import numpy as np
+import matplotlib.pyplot as plt
+from os.path import join
+import selectivesearch
+from cnn import class_names
+import cv2
+from nms import nms
+import re
+
+
+TEST_SELECTIVE_SEARCH_IMAGE_PATH = "../test-detection"
+clf = load_model("cnn.hdf5")
+SHOW_IMAGES = True
+
+
+HITS_NUM = 0
+MISSES_NUM = 0
+NUMBER_OF_TRIES = 0
+
+
+def show_images(img):
+    if SHOW_IMAGES:
+        plt.imshow(img)
+        plt.show()
+
+
+def get_object_proposals(img, scale=500, sigma=0.9, min_size=10):
+    # Selective search
+    img_lbl, regions = selectivesearch.selective_search(
+        img, scale=scale, sigma=sigma, min_size=min_size)
+
+    candidates = set()
+    for r in regions:
+        # excluding same rectangle (with different segments)
+        if r['rect'] in candidates:
+            continue
+        # excluding regions smaller than 500 pixels
+        x, y, w, h = r['rect']
+        if r['size'] < 2000 or w > 0.95 * img.shape[1] or h > 0.95 * img.shape[0]:
+            continue
+        # excluding the zero-width or zero-height box
+        if r['rect'][2] == 0 or r['rect'][3] == 0:
+            continue
+        # distorted rects
+        if w / h > 5 or h / w > 5:
+            continue
+        candidates.add(r['rect'])
+        # print(r)
+
+    return candidates
+
+
+def get_images_and_regions(image_path):
+    full_img = read_image(image_path)
+    regions = get_object_proposals(full_img)
+    # print(len(regions))
+    region_list = []
+    images = []
+    for r in regions:
+        region_list.append(r)
+        x, y, w, h = r
+        img = full_img[y:y+h, x:x+w]
+        images.append(img)
+        region_list.append(r)
+    return np.array(images), np.array(region_list)
+
+
+# def find_logo(image_path):
+#     image = read_image(image_path)
+#     clone = image.copy()
+#     images, regions = get_images_and_regions(image_path)
+#     # for img in images:
+#     for i in xrange(len(images)):
+#         print(i)
+#         img = images[i]
+#         r = regions[i]
+#         res_img = resize_and_save(img, "")
+#         res_img = np.array(res_img, np.float32)
+#         res_img /= 255
+#         pred_class_ind = clf.predict_classes(np.array([res_img]))[0]
+#         predictions = clf.predict(np.array([res_img]))[0][pred_class_ind]
+#         pred_class_name = class_names[pred_class_ind]
+#         print(pred_class_name)
+#         # pred_class_name = class_names[]
+#         if pred_class_name != "None":
+#             x, y, w, h = r
+#             print(r)
+#             plt.imshow(clone[y:y+h, x:x+w])
+#             plt.show()
+#             cv2.rectangle(clone, (x, y), (x+w, y+h), (255, 0, 0), thickness=2)
+#             plt.imshow(img)
+#             plt.show()
+#             print("Predicted class: " + pred_class_name)
+#     plt.imshow(clone)
+#     plt.show()
+
+def find_logo(image_path):
+    image = read_image(image_path)
+    clone = image.copy()
+    regions = get_object_proposals(image)
+
+    detections = []
+
+    for r in regions:
+        x, y, w, h = r
+        img = image[y:y+h, x:x+w]
+        res_img = resize_and_save(img, "")
+        res_img = np.array(res_img, np.float32)
+        res_img /= 255
+        pred_class_ind = clf.predict_classes(np.array([res_img]))[0]
+        predictions = clf.predict(np.array([res_img]))[0][pred_class_ind]
+        pred_class_name = class_names[pred_class_ind]
+        if pred_class_name != "None":
+            print("Prediceted classes: %s" % pred_class_name)
+            # print(r)
+            print("Sure for this: %f" % predictions)
+            cv2.rectangle(clone, (x, y), (x+w, y+h), (255, 255, 0), thickness=5)
+            detections.append((x, y, predictions, w, h, pred_class_name))
+    print("before nms")
+    # plt.imshow(clone)
+    # plt.show()
+    #
+    show_images(clone)
+
+    detections = nms(detections, 0.3)
+    found = set()
+    clone = image.copy()
+    for d in detections:
+        x, y, _, w, h, class_name = d
+        cv2.rectangle(clone, (x, y), (x + w, y + h), (255, 255, 0), thickness=5)
+        found.add(class_name)
+    print("after nms")
+    # plt.imshow(clone)
+    # plt.show()
+    show_images(clone)
+
+    return found
+
+
+def test_all_images():
+    global SHOW_IMAGES
+    global HITS_NUM
+    global MISSES_NUM
+    global NUMBER_OF_TRIES
+    HITS_NUM = 0
+    MISSES_NUM = 0
+    NUMBER_OF_TRIES = 0
+    SHOW_IMAGES = False
+    with open(join(TEST_SELECTIVE_SEARCH_IMAGE_PATH, "labels.txt")) as annot:
+        for line in annot.readlines():
+            if line.strip() == "":
+                continue
+            elif "=" in line:
+                break
+            img_rel_path, class_name = re.split("\\s+", line.strip())
+            print("Looking for logo at image: %s" % img_rel_path)
+            found = find_logo(join(TEST_SELECTIVE_SEARCH_IMAGE_PATH, img_rel_path.strip()))
+            # found = set()
+            # da li smo pogodili
+            hit_num = 0
+            # da li smo pogodili
+            if class_name in found:
+                hit_num += 1
+            # koliko smo promasili
+            miss_num = len(found) - hit_num
+            if miss_num > 0:
+                print("****************************************Greska prilikom trazenja loga na slic: " + img_rel_path)
+            print("=" * 33)
+            print("Number of hits for %s is %d" % (img_rel_path, hit_num))
+            print("Number of misses for %s is %d" % (img_rel_path, miss_num))
+            print("=" * 33)
+            HITS_NUM += hit_num
+            MISSES_NUM += miss_num
+            NUMBER_OF_TRIES += 1
+    print("\n\n\n")
+    print("Number of tries: %d" % NUMBER_OF_TRIES)
+    print("Number of hits: %d" % HITS_NUM)
+    print("Number of misses: %d" % MISSES_NUM)
+
+
+def main():
+    global TEST_SELECTIVE_SEARCH_IMAGE_PATH
+    # find_logo(join(TEST_SELECTIVE_SEARCH_IMAGE_PATH, "adidas1.jpg"))
+    # find_logo(join(TEST_SELECTIVE_SEARCH_IMAGE_PATH, "yahoo2.jpg"))
+    # found = find_logo(join(TEST_SELECTIVE_SEARCH_IMAGE_PATH, "nike123.jpg"))
+    # find_logo(join(TEST_SELECTIVE_SEARCH_IMAGE_PATH, "adidas2.jpg"))
+    # found = find_logo(join(TEST_SELECTIVE_SEARCH_IMAGE_PATH, "bmw3.jpg"))
+    # found = find_logo(join(TEST_SELECTIVE_SEARCH_IMAGE_PATH, "minibmw.jpg"))
+    # found = find_logo(join(TEST_SELECTIVE_SEARCH_IMAGE_PATH, "puma3.jpg"))
+    # found = find_logo(join(TEST_SELECTIVE_SEARCH_IMAGE_PATH, "minibmw1.jpg"))
+    # print(found)
+    test_all_images()
+
+
+if __name__ == '__main__':
+    main()
